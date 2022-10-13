8f3459cca8c00fe1877f82e6a952f5e9effb826e
CarstenSavage47 <rampagev2@gmail.com>
2022-10-13 09:33:26 -0700

    Added SQL queries

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# This is a sample Python script.
+# This is a demo of SQL functions using PySpark.
 
 import pandas
 import pyspark
@@ -18,47 +18,14 @@ Employees_Spark.show()
 Orders_Spark.createOrReplaceTempView("Orders_Spark")
 Employees_Spark.createOrReplaceTempView("Employees_Spark")
 
-EmployeeOrders_Spark = spark.sql("SELECT * from Orders_Spark a LEFT JOIN Employees_Spark b ON a.id = b.id")
+EmployeeOrdersInner_Spark = spark.sql("SELECT * from Orders_Spark a INNER JOIN Employees_Spark b USING (id)")
 
-EmployeeOrders_Spark.show()
+EmployeeOrdersInner_Spark.show()
 
-EmployeeOrders = EmployeeOrders_Spark.toPandas()
+EmployeeOrdersInner_Spark.createOrReplaceTempView("EmployeeOrdersInner_Spark")
 
+Employee_Cost_Rank = spark.sql("SELECT *, DENSE_RANK() OVER (ORDER BY item_cost desc) AS Cost_RANK FROM EmployeeOrdersInner_Spark ORDER BY firstname asc")
 
+Employee_Cost_Rank.createOrReplaceTempView("Employee_Cost_Rank")
 
-
-
-
-Diamonds = pandas.read_csv('/Users/carstenjuliansavage/Desktop/R Working Directory/diamonds.csv')
-
-Diamonds_Spark = spark.createDataFrame(Diamonds)
-
-Diamonds_Spark.show()
-
-Diamonds_Spark.printSchema()
-
-Diamonds_Spark.show(5)
-
-Diamonds_Spark.show(2, vertical=True)
-
-(Diamonds_Spark
- .select(["x", "y", "z"])
- .describe()
- .show()
- )
-
-Diamonds_Spark.collect()
-
-#Diamonds_Pandas = Diamonds_Spark.toPandas()
-
-Diamonds_Spark.select(Diamonds_Spark.carat).show()
-import numpy
-Diamonds_Spark.withColumn('carat2', 2*(Diamonds_Spark.carat)).show()
-
-Diamonds_Spark.filter(Diamonds_Spark.x > 4).show()
-
-Diamonds_Spark.groupby(['carat','cut']).avg().show()
-
-Diamonds_Spark.createOrReplaceTempView("DIASPARK")
-spark.sql("SELECT count(*) from DIASPARK").show()
-spark.sql("SELECT * from DIASPARK WHERE CUT LIKE 'I%'").show()
+Employee_Cost_Rank = Employee_Cost_Rank.toPandas()
