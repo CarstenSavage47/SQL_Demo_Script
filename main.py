631f21fb561ef998865e8e6f9f95edbc7a27b8eb
CarstenSavage47 <rampagev2@gmail.com>
2022-10-14 14:45:00 -0700

    Code formatting

--- main.py
+++ main.py
@@ -17,14 +17,25 @@ Employees_Spark.show()
 Orders_Spark.createOrReplaceTempView("Orders_Spark")
 Employees_Spark.createOrReplaceTempView("Employees_Spark")
 
-EmployeeOrdersInner_Spark = spark.sql("SELECT * from Orders_Spark a INNER JOIN Employees_Spark b USING (id)")
+EmployeeOrdersInner_Spark = spark.sql("SELECT * "
+                                      "FROM Orders_Spark a "
+                                      "INNER JOIN Employees_Spark b "
+                                      "USING (id)")
 
 EmployeeOrdersInner_Spark.show()
 
 EmployeeOrdersInner_Spark.createOrReplaceTempView("EmployeeOrdersInner_Spark")
 
-Employee_Cost_Rank = spark.sql("SELECT *, DENSE_RANK() OVER (ORDER BY item_cost desc) AS Cost_RANK FROM EmployeeOrdersInner_Spark ORDER BY firstname asc")
+# Dense Rank
+Employee_Cost_Rank = spark.sql("SELECT *, DENSE_RANK() OVER (ORDER BY item_cost desc) AS Cost_RANK "
+                               "FROM EmployeeOrdersInner_Spark "
+                               "ORDER BY firstname asc")
 
 Employee_Cost_Rank.createOrReplaceTempView("Employee_Cost_Rank")
 
 Employee_Cost_Final = Employee_Cost_Rank.toPandas()
+
+# Rank
+spark.sql("SELECT *, RANK() OVER (PARTITION BY firstname ORDER BY item_cost desc) AS Cost_RANK "
+          "FROM EmployeeOrdersInner_Spark "
+          "ORDER BY firstname, item_cost desc").show()
