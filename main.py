d9a139e2b729ad2b9ba4738999654b513260d3ca
CarstenSavage47 <rampagev2@gmail.com>
2022-10-17 20:29:10 -0700

    Added Snowflake implementation example

--- main.py
+++ main.py
@@ -5,15 +5,40 @@ import pyspark
 from pyspark.sql import SparkSession
 from pyspark.sql import Row
 
+#   Simulated Snowflake Implementation
+
+#   use warehouse EMPORDERS_WAREHOUSE
+#   use database EMPORDERS_DATABASE
+#   use schema PUBLIC
+
+#   create table "Orders" (
+#   id numeric,
+#   item string,
+#   quantity_ordered numeric,
+#   item_cost numeric
+#   );
+
+#   create table "Employees" (
+#   id numeric,
+#   lastname string,
+#   firstname string,
+#   gender string
+#   );
+
+# Create a Spark session
 spark = SparkSession.builder.getOrCreate()
+
 
 Orders = pandas.read_csv('/Users/carstenjuliansavage/Desktop/R Working Directory/r-sql-demo-files/orders.csv')
 Employees = pandas.read_csv('/Users/carstenjuliansavage/Desktop/R Working Directory/r-sql-demo-files/employees.csv')
 
+# Create spark instances of the tables
 Orders_Spark = spark.createDataFrame(Orders)
 Employees_Spark = spark.createDataFrame(Employees)
 Orders_Spark.show()
 Employees_Spark.show()
+
+# Create referenceable tables
 Orders_Spark.createOrReplaceTempView("Orders_Spark")
 Employees_Spark.createOrReplaceTempView("Employees_Spark")
 
